c3f6b616fdd67181e02c88cbdf578829f8115a7b
Muhibbulloh H. <255422806+hmuhibbulloh@users.noreply.github.com>
2026-05-14 15:45:51 +0500

    Generate cycle based on training max, not actual one rep max

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 // HELPER FUNCTIONS
-function calculateTrainingMax(oneRepMax) {
-  return oneRepMax * 0.9;
-}
-
 function roundDownToNearest10(workingSet) {
   let floored = Math.floor(workingSet);
   let remainder = floored % 10;
@@ -24,31 +20,136 @@ const calculateBtn = document.querySelector(".btn--calc");
 const ohpInputEl = document.querySelector("#ohp");
 const sqtInputEl = document.querySelector("#sqt");
 const dltInputEl = document.querySelector("#dlt");
-
-calculateBtn.addEventListener("click", generateCycle);
+const boardEl = document.querySelector(".board");
+calculateBtn.addEventListener("click", renderUI);
 
 function generateCycle() {
-  const ohpTM = calculateTrainingMax(parseFloat(ohpInputEl.value));
-  const sqtTM = calculateTrainingMax(parseFloat(sqtInputEl.value));
-  const dltTM = calculateTrainingMax(parseFloat(dltInputEl.value));
+  const ohpTM = parseFloat(ohpInputEl.value);
+  const sqtTM = parseFloat(sqtInputEl.value);
+  const dltTM = parseFloat(dltInputEl.value);
 
-  const cycle = {
-    ohp: {
-      week1: [WEEK1.map((p) => calculateWorkingSet(ohpTM, p))],
-      week2: [WEEK2.map((p) => calculateWorkingSet(ohpTM, p))],
-      week3: [WEEK3.map((p) => calculateWorkingSet(ohpTM, p))],
-    },
-    sqt: {
-      week1: [WEEK1.map((p) => calculateWorkingSet(sqtTM, p))],
-      week2: [WEEK2.map((p) => calculateWorkingSet(sqtTM, p))],
-      week3: [WEEK3.map((p) => calculateWorkingSet(sqtTM, p))],
-    },
-    dlt: {
-      week1: [WEEK1.map((p) => calculateWorkingSet(dltTM, p))],
-      week2: [WEEK2.map((p) => calculateWorkingSet(dltTM, p))],
-      week3: [WEEK3.map((p) => calculateWorkingSet(dltTM, p))],
-    },
-  };
+  if (isNaN(ohpTM) || isNaN(sqtTM) || isNaN(dltTM)) {
+    return false;
+  } else {
+    const cycle = {
+      ohp: {
+        week1: WEEK1.map((p) => calculateWorkingSet(ohpTM, p)),
+        week2: WEEK2.map((p) => calculateWorkingSet(ohpTM, p)),
+        week3: WEEK3.map((p) => calculateWorkingSet(ohpTM, p)),
+      },
+      sqt: {
+        week1: WEEK1.map((p) => calculateWorkingSet(sqtTM, p)),
+        week2: WEEK2.map((p) => calculateWorkingSet(sqtTM, p)),
+        week3: WEEK3.map((p) => calculateWorkingSet(sqtTM, p)),
+      },
+      dlt: {
+        week1: WEEK1.map((p) => calculateWorkingSet(dltTM, p)),
+        week2: WEEK2.map((p) => calculateWorkingSet(dltTM, p)),
+        week3: WEEK3.map((p) => calculateWorkingSet(dltTM, p)),
+      },
+    };
 
-  console.log(cycle);
+    return cycle;
+  }
+}
+
+function renderUI() {
+  const cycle = generateCycle();
+  if (cycle) {
+    let htmlW1 = `
+    <h1>Week 1 (5/5/5+)</h1>
+    <div class="flex">
+      <div class="exercise">
+        <p>Overhead Press</p>
+        <ul>
+          <li>Set 1: ${cycle.ohp.week1[0]}kg</li>
+          <li>Set 2: ${cycle.ohp.week1[1]}kg</li>
+          <li>Set 3: ${cycle.ohp.week1[2]}kg</li>
+        </ul>
+      </div>
+        <div class="exercise">
+        <p>Squat</p>
+        <ul>
+          <li>Set 1: ${cycle.sqt.week1[0]}kg</li>
+          <li>Set 2: ${cycle.sqt.week1[1]}kg</li>
+          <li>Set 3: ${cycle.sqt.week1[2]}kg</li>
+        </ul>
+      </div>
+      <div class="exercise">
+        <p>Deadlift</p>
+        <ul>
+          <li>Set 1: ${cycle.dlt.week1[0]}kg</li>
+          <li>Set 2: ${cycle.dlt.week1[1]}kg</li>
+          <li>Set 3: ${cycle.dlt.week1[2]}kg</li>
+        </ul>
+      </div>
+    </div>
+    `;
+
+    let htmlW2 = `
+    <h1>Week 2 (3/3/3+)</h1>
+    <div class="flex">
+      <div class="exercise">
+        <p>Overhead Press</p>
+        <ul>
+          <li>Set 1: ${cycle.ohp.week2[0]}kg</li>
+          <li>Set 2: ${cycle.ohp.week2[1]}kg</li>
+          <li>Set 3: ${cycle.ohp.week2[2]}kg</li>
+        </ul>
+      </div>
+        <div class="exercise">
+        <p>Squat</p>
+        <ul>
+          <li>Set 1: ${cycle.sqt.week2[0]}kg</li>
+          <li>Set 2: ${cycle.sqt.week2[1]}kg</li>
+          <li>Set 3: ${cycle.sqt.week2[2]}kg</li>
+        </ul>
+      </div>
+      <div class="exercise">
+        <p>Deadlift</p>
+        <ul>
+          <li>Set 1: ${cycle.dlt.week2[0]}kg</li>
+          <li>Set 2: ${cycle.dlt.week2[1]}kg</li>
+          <li>Set 3: ${cycle.dlt.week2[2]}kg</li>
+        </ul>
+      </div>
+    </div>
+    `;
+
+    let htmlW3 = `
+    <h1>Week 3 (5/3/1+)</h1>
+    <div class="flex">
+      <div class="exercise">
+        <p>Overhead Press</p>
+        <ul>
+          <li>Set 1: ${cycle.ohp.week3[0]}kg</li>
+          <li>Set 2: ${cycle.ohp.week3[1]}kg</li>
+          <li>Set 3: ${cycle.ohp.week3[2]}kg</li>
+        </ul>
+      </div>
+        <div class="exercise">
+        <p>Squat</p>
+        <ul>
+          <li>Set 1: ${cycle.sqt.week3[0]}kg</li>
+          <li>Set 2: ${cycle.sqt.week3[1]}kg</li>
+          <li>Set 3: ${cycle.sqt.week3[2]}kg</li>
+        </ul>
+      </div>
+      <div class="exercise">
+        <p>Deadlift</p>
+        <ul>
+          <li>Set 1: ${cycle.dlt.week3[0]}kg</li>
+          <li>Set 2: ${cycle.dlt.week3[1]}kg</li>
+          <li>Set 3: ${cycle.dlt.week3[2]}kg</li>
+        </ul>
+      </div>
+    </div>
+    `;
+    boardEl.classList.add("visible");
+    boardEl.innerHTML += htmlW1;
+    boardEl.innerHTML += htmlW2;
+    boardEl.innerHTML += htmlW3;
+  } else {
+    alert("Enter valid numbers");
+  }
 }
